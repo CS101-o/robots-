ffcb653ad14fee90d2045cc214c0092ea8366f61
Zawye77 <161248184+Zawye77@users.noreply.github.com>
2024-03-16 22:37:44 +0000

    Add files via upload

--- Test_1.ino
+++ Test_1.ino
@@ -7,10 +7,10 @@ int Motor_left_direction = 6;   //   0 Forward - 1 Reverse
 
 int Left_encoder = 2;
 int Right_encoder = 3;
-int Left_forward_speed=0;
-int Right_forward_speed=0;
-int Left_reverse_speed=255;
-int Right_reverse_speed=255;
+int Left_forward_speed=200;
+int Right_forward_speed=200;
+int Left_reverse_speed=200;
+int Right_reverse_speed=200;
 
 int direction = 0;
 float right_dir;
@@ -24,30 +24,9 @@ void forward() {
 }
 
 void Stop(){ // set speeds to 0
-  analogWrite(Motor_right_PWM,255); // right motor
-  digitalWrite(Motor_right_direction,Forward);
-  analogWrite(Motor_left_PWM,255); // left 
-  digitalWrite(Motor_left_direction,Forward); //left
+  analogWrite(Motor_right_PWM,0); // right motor
+  analogWrite(Motor_left_PWM,0); // left 
   //MsTimer2::start();
-}
-
-void right(){    
-  analogWrite(Motor_right_PWM,Right_forward_speed);
-  digitalWrite(Motor_right_direction,Forward);
-  analogWrite(Motor_left_PWM,Left_reverse_speed); // left 
-  digitalWrite(Motor_left_direction,Reverse); //left
-
-  if (right_dir >= 90.0) {
-    Stop();
-    right_dir = 0;
-    direction++;
-  }
-  if (direction >= 3) {
-      direction = 0;
-    }
-    else {
-      direction++;
-  }
 }
 
 void setup() {
@@ -57,12 +36,8 @@ void setup() {
 }
 
 void loop() {
-  right();
-  //forward();
-  delay(5000);
+  forward();
+  delay(500);
   Stop();
-  delay(5000);
 }
-
-
 
